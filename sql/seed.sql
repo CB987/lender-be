a0ee0e3022b75f8dcad43c3cdfac21c21d0fe40d
Clare Barton <grimalkinclare@gmail.com>
2018-11-14 13:32:44 -0500

    adds false sql table data

--- sql/seed.sql
+++ sql/seed.sql
@@ -15,4 +15,36 @@ values
     ('decorations/ holiday items'),
 
     insert into users
-        (name, 
+        (name, username, email, city, state)
+    values
+        ('Clare', 'cb23', 'clare@email.com', 'Mableton','GA'),
+        ('Amelia', 'as12', 'amelia@email.com', 'Decatur','GA'),
+        ('Steven', 'sk34', 'steven@email.com', 'Johns Creek','GA'),
+        ('Alex', 'ar45', 'alex@email.com', 'town','GA'),
+        ('Anokhee', 'aj56', 'anokhee@email.com', 'town', 'GA'),
+        ('April', 'ac67', 'april@email.com', 'town', 'GA'),
+        ('Aylin', 'ad78', 'aylin@email.com', 'town', 'GA'),
+        ('Beth', 'bd89', 'beth@email.com', 'town', 'GA'),
+        ('Evan', 'ep90', 'evan@email.com', 'town', 'GA'),
+        ('Greg', 'gf01', 'greg@email.com', 'town', 'GA'),
+        ('Helen', 'hh09', 'helen@email.com', 'town', 'GA'),
+        ('Ian', 'ih98', 'ian@email.com', 'town', 'GA'),
+        ('Isaac', 'is87', 'isaac@email.com', 'town', 'GA'),
+        ('Jake', 'jh76', 'jake@email.com', 'town', 'GA'),
+        ('Liz', 'lc65', 'liz@email.com', 'town', 'GA'),
+        ('Stan', 'sc54', 'stan@email.com', 'town', 'GA'),
+        ('Brien', 'bm43', 'brien@email.com', 'town', 'GA'),
+        ('Carlos', 'cr32', 'carlos@email.com', 'town', 'GA'),
+        ('ChrisA', 'cookiepants', 'chrisa@email.com', 'town', 'GA'),
+        ('Collin', 'ca21', 'collin@email.com', 'town', 'GA'),
+        ('Ed', 'ew10', 'ed@email.com', 'town', 'GA'),
+        ('Kyle', 'ks01', 'kyle@email.com', 'town', 'GA'),
+        ('Lorenzo', 'ls12', 'lorenzo@email.com', 'town', 'GA'),
+        ('Max', 'mm23', 'max@email.com', 'town', 'GA'),
+        ('Michael', 'mb34', 'michael@email.com', 'town', 'GA'),
+        ('Quinton', 'qm45', 'quinton@email.com', 'town', 'GA'),
+        ('Sam', 'se56', 'sam@email.com', 'town', 'GA'),
+        ('Will', 'wh67', 'will@email.com', 'town', 'GA'),
+        ('ChrisF', 'cf78', 'chrisf@email.com', 'town', 'GA'),
+        ('Melon', 'mh89', 'melon@email.com', 'town,' 'GA')
+        
