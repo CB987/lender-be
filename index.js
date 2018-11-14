65ddddfc9a038229fb844c0610c4dcf52e572fca
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-11-14 13:50:52 -0500

    linking body parser

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
+
 const express = require('express'); 
 const app = express(); 
 app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
